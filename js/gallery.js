4306408c4cce0c851f5861834b34513c3c3e4d71
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-02-02 15:06:40 +0100

    Clear unnecessary consol.log calls

--- js/gallery.js
+++ js/gallery.js
@@ -1,73 +1,70 @@
 import imagesSource from './gallery-items.js';
 
 const refs = {
-	gallery: document.querySelector('.js-gallery'),
-	largeImage: document.querySelector('.lightbox__image'),
-	lightbox: document.querySelector('.js-lightbox'),
-	closeBtn: document.querySelector('[data-action="close-lightbox"]'),
-}
+  gallery: document.querySelector('.js-gallery'),
+  largeImage: document.querySelector('.lightbox__image'),
+  lightbox: document.querySelector('.js-lightbox'),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
+};
 
 imagesSource.forEach((image, index) => {
-	const imgageMarkup = `<li><a class='gallery__link' href='${image.original}'><img class='gallery__image' src='${image.preview}' data-source='${image.original}' data-index='${index}' alt='${image.description}'/></a></li>`;
-	refs.gallery.insertAdjacentHTML('beforeend', imgageMarkup);
-})
+  const imageMarkup = `<li><a class='gallery__link' href='${image.original}'><img class='gallery__image' src='${image.preview}' data-source='${image.original}' data-index='${index}' alt='${image.description}'/></a></li>`;
+  refs.gallery.insertAdjacentHTML('beforeend', imageMarkup);
+});
 
 refs.gallery.addEventListener('click', onGalleryClick);
 refs.closeBtn.addEventListener('click', onCloseModal);
 refs.lightbox.addEventListener('click', onLightboxClick);
 
 function onGalleryClick(event) {
-	event.preventDefault();
-	console.log(event.target.alt);
-	if(event.target.nodeName !== 'IMG') return
-	refs.largeImage.src = event.target.dataset.source;
-	refs.largeImage.alt = event.target.alt;
-	onOpenModal();
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+  refs.largeImage.src = event.target.dataset.source;
+  refs.largeImage.alt = event.target.alt;
+  onOpenModal();
 }
 
 function onOpenModal() {
-	refs.lightbox.classList.add('is-open');
-	window.addEventListener('keydown', onEscPress);
-	window.addEventListener('keydown', onArrowClick);
+  refs.lightbox.classList.add('is-open');
+  window.addEventListener('keydown', onEscPress);
+  window.addEventListener('keydown', onArrowClick);
 }
 
 function onCloseModal() {
-	refs.lightbox.classList.remove('is-open');
-	refs.largeImage.src = '';
-	refs.largeImage.alt = '';
-	window.removeEventListener('keydown', onEscPress);
-	window.removeEventListener('keydown', onArrowClick);
+  refs.lightbox.classList.remove('is-open');
+  refs.largeImage.src = '';
+  refs.largeImage.alt = '';
+  window.removeEventListener('keydown', onEscPress);
+  window.removeEventListener('keydown', onArrowClick);
 }
 
 function onLightboxClick(event) {
-	if (event.currentTarget.nodeName === event.target.nodeName) {
-		onCloseModal();
-	};
+  if (event.currentTarget.nodeName === event.target.nodeName) {
+    onCloseModal();
+  }
 }
 
 function onEscPress(event) {
-	if (event.code === 'Escape') {
-		onCloseModal();
-	};
+  if (event.code === 'Escape') {
+    onCloseModal();
+  }
 }
 
 function onArrowClick(event) {
-	let index = 0;
-		index = +event.target.firstElementChild.dataset.index;
-	console.log('before index', index);
-	if (event.code === 'ArrowLeft' && index > 0) {
-		index -= 1;
-		slider(event, index);
-	}
-	if (event.code === 'ArrowRight' && index < imagesSource.length-1) {
-		index += 1;
-		slider(event, index);
-	}
-	console.log('after index', index);
+  let index = 0;
+  index = +event.target.firstElementChild.dataset.index;
+  if (event.code === 'ArrowLeft' && index > 0) {
+    index -= 1;
+    slider(event, index);
+  }
+  if (event.code === 'ArrowRight' && index < imagesSource.length - 1) {
+    index += 1;
+    slider(event, index);
+  }
 }
-	
+
 function slider(event, index) {
-	event.target.firstElementChild.dataset.index = index;
-	refs.largeImage.src = imagesSource[index].original;
-	refs.largeImage.alt = imagesSource[index].description;
+  event.target.firstElementChild.dataset.index = index;
+  refs.largeImage.src = imagesSource[index].original;
+  refs.largeImage.alt = imagesSource[index].description;
 }
